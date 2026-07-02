f1dedec3e5dd6c73e780952f66c0d364e48ffe64
JuliusLJ <julius.johansson0@gmail.com>
2026-07-03 02:44:18 +0300

    Added delay of 5mins to roster changes due to req/minute limit

--- src/pages/api/team-rosters.ts
+++ src/pages/api/team-rosters.ts
@@ -279,7 +279,7 @@ export const GET: APIRoute = async () => {
         status: 200,
         headers: {
           'Content-Type': 'application/json',
-          'Cache-Control': 'no-store',
+          'Cache-Control': 'public, max-age=300'
         },
       }
     );
