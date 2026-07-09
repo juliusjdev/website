474d82cd265e2b7776141ec7b8b5811e0bb929de
JuliusLJ <julius.johansson0@gmail.com>
2026-07-10 00:49:53 +0300

    forgot to include file on last fix

--- src/pages/api/stats.ts
+++ src/pages/api/stats.ts
@@ -188,7 +188,7 @@ export const GET: APIRoute = async () => {
         status: 200,
         headers: {
           'Content-Type': 'application/json',
-          'Cache-Control': 'no-store',
+          'Cache-Control': 'public, max-age=300',
         },
       }
     );
